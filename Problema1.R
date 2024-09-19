c2cccc67775c79c6d959987d263f02c8123631d0
marc.martinez.luis <marc.martinez.luis@auleseebe.upc.edu>
2024-09-19 10:53:28 +0200

    proba2

--- Problema1.R
+++ Problema1.R
@@ -1,5 +1,15 @@
 #Codigo para problema 1
+mis_dades <- mtcars
+mis_dades
+#mitjana qsec
+mis_dades$qsec
+#ordenar
+sort(mis_dades$qsec)
+#tallarlo en intervals + dir quants temps hi han a cada interval
+cut(mis_dades$qsec, breaks=4)
+qsec_intervalos <- cut(mis_dades$qsec, breaks=4)
+qsec_intervalos
+table(qsec_intervalos)
 
-plot(1:5)
-2+2
-
+hist(mis_dades$qsec, breaks= 4)
+     
